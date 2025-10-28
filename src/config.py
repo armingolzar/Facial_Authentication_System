d1978029f85df50f335d8b4583feade868d8fb6d
Armin Golzar <armingolzar78@gmail.com>
2025-10-28 19:49:24 +0330

    infrence is done with 0.5 threshold on 166800 test samples

--- src/config.py
+++ src/config.py
@@ -13,3 +13,12 @@ DATA_TEST_PATH = ".\\data\\classification\\test1000\\*\\*.*"
 MODELS_PATH = os.path.join(".", "models", "siamese_face_v1_contrastive.h5")
 
 PLOTS_PATH = os.path.join(".", "assets", "learning_plot_contrastive.png")
+
+
+CSV_TEST_PATH = ".\\data\\verification\\verification_dev.csv"
+
+MODEL_CLASSIFICATION_PATH = ".\\models\\siamese_face_v1_classification.h5"
+
+MODEL_CONTRASTIVE_PATH = ".\\models\\siamese_face_v1_contrastive.h5"
+
+THRESHOLD = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
--- src/infrence.py
+++ src/infrence.py
@@ -8,11 +8,6 @@ from src.model import embedding_extractor
 import src.config as config
 
 
-verification_dev_path = ".\\data\\verification\\verification_dev.csv"
-MODEL_CLASSIFICATION_PATH = ".\\models\\siamese_face_v1_classification.h5"
-MODEL_CONTRASTIVE_PATH = ".\\models\\siamese_face_v1_contrastive.h5"
-
-
 def model_arch(mode="contrastive"):
 
     if mode == "contrastive":
@@ -26,7 +21,7 @@ def model_arch(mode="contrastive"):
         distance = tf.keras.layers.Lambda(euclidean_distance)([embeddedA, embeddedB])
         # output = Dense(1, activation="sigmoid")(distance)
         siamese_face_model = tf.keras.models.Model(inputs=[inputA, inputB], outputs=distance)
-        siamese_face_model.load_weights(MODEL_CONTRASTIVE_PATH)
+        siamese_face_model.load_weights(config.MODEL_CONTRASTIVE_PATH)
 
     elif mode == "classification":
         inputA = tf.keras.layers.Input(config.IMAGE_SHAPE, name="inputA")
@@ -39,7 +34,7 @@ def model_arch(mode="contrastive"):
         distance = tf.keras.layers.Lambda(euclidean_distance)([embeddedA, embeddedB])
         output = tf.keras.layers.Dense(1, activation="sigmoid")(distance)
         siamese_face_model = tf.keras.models.Model(inputs=[inputA, inputB], outputs=output)
-        siamese_face_model.load_weights(MODEL_CLASSIFICATION_PATH)
+        siamese_face_model.load_weights(config.MODEL_CLASSIFICATION_PATH)
 
     else:
         siamese_face_model = None
@@ -50,45 +45,59 @@ def model_arch(mode="contrastive"):
 
 def process_to_image(img_path):
 
-    img = tf.io.read_file(img_path)
+    img = tf.io.read_file(os.path.join(".\\data\\verification", img_path))
     img = tf.image.decode_jpeg(img, channels=3)
     img = tf.image.resize(img, [224, 224])
     img = tf.cast(img, tf.float32) / 255.0
 
     return img
 
+def generator(imgA_path, imgB_path, labels):
 
-
-
-
-
-
-
-
+    for imgA, imgB, label in zip(imgA_path, imgB_path, labels):
+        yield(process_to_image(imgA), process_to_image(imgB), label)
 
 
 def face_authentication_system(data_path, mode="contrastive", threshold=0.5):
 
+    y_true, y_pred = [], []
     data = pd.read_csv(data_path)
     siamese_model = model_arch(mode)
     path_imageA = data.iloc[:,  0].values
     path_imageB = data.iloc[:,  1].values
-    label = data.iloc[:, -1].values
+    labels = data.iloc[:, -1].values
+
+    dataset = tf.data.Dataset.from_generator(
+                lambda : generator(path_imageA, path_imageB, labels), 
+                output_signature=(
+                    tf.TensorSpec(shape=config.IMAGE_SHAPE, dtype=tf.float32),
+                    tf.TensorSpec(shape=config.IMAGE_SHAPE, dtype=tf.float32),
+                    tf.TensorSpec(shape=(), dtype=tf.int32)
+                )
+    )
+    dataset = dataset.batch(config.BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+
+    for imgA_batch, imgB_batch, labels_batch in dataset:
+        distance = siamese_model([imgA_batch, imgB_batch], training=False)
+        preds_batch = tf.cast(distance < config.THRESHOLD, tf.int32)
+        y_pred.extend(preds_batch.numpy().flatten())
+        y_true.extend(labels_batch.numpy())
 
 
+    return y_true, y_pred
 
 
+y_true, y_pred = face_authentication_system(config.CSV_TEST_PATH)
+
+print("y_true[:20]:", y_true[:20])
+print("y_pred[:20]:", y_pred[:20])
 
 
+accuracy = accuracy_score(y_true, y_pred)
+cm = confusion_matrix(y_true, y_pred)
 
-
-
-
-path_imageA, path_imageB, label = face_authentication_system(verification_dev_path)
-
-
-
-
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", cm)
 
 
 
